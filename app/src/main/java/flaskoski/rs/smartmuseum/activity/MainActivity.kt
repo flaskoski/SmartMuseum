2f182dbbb203c3ea1d3d6e3233a5962185636c17
flaskoski <felipeflaskoski@outlook.com>
2019-06-02 21:12:15 -0300

    user location functions extracted to UserLocationManager class

--- app/src/main/java/flaskoski/rs/smartmuseum/activity/MainActivity.kt
+++ app/src/main/java/flaskoski/rs/smartmuseum/activity/MainActivity.kt
@@ -1,17 +1,12 @@
 package flaskoski.rs.smartmuseum.activity
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.IntentSender
 import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
 import android.location.LocationManager
-import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomSheetBehavior
 import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
 import android.view.Menu
@@ -19,11 +14,9 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.*
-import com.google.android.gms.tasks.Task
 import flaskoski.rs.rs_cf_test.recommender.RecommenderBuilder
 import flaskoski.rs.smartmuseum.DAO.ItemDAO
 import flaskoski.rs.smartmuseum.DAO.RatingDAO
@@ -32,7 +25,9 @@ import flaskoski.rs.smartmuseum.R
 import flaskoski.rs.smartmuseum.listAdapter.ItemsGridListAdapter
 import flaskoski.rs.smartmuseum.model.Item
 import flaskoski.rs.smartmuseum.model.Rating
+import flaskoski.rs.smartmuseum.model.UserLocationManager
 import flaskoski.rs.smartmuseum.recommender.RecommenderManager
+//import flaskoski.rs.smartmuseum.location
 import flaskoski.rs.smartmuseum.util.ApplicationProperties
 import kotlinx.android.synthetic.main.activity_main_bottom_sheet.*
 import java.util.*
@@ -41,8 +36,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
 
     private val REQUEST_GET_PREFERENCES: Int = 1
     private val REQUEST_ITEM_RATING_CHANGE: Int = 2
-    private val REQUEST_CHECK_LOCATION_SETTINGS: Int = 3
+    val REQUEST_CHANGE_LOCATION_SETTINGS: Int = 3
 
+    private var userLocationManager : UserLocationManager? = null
     private val itemsList = ArrayList<Item>()
     private var ratingsList  = HashSet<Rating>()
     private val TAG = "MainActivity"
@@ -67,29 +63,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
     private lateinit var parallelRequestsManager: ParallelRequestsManager
 
     private var mMap: GoogleMap? = null
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
-    private var mCurrLocationMarker : Marker? = null
-    private var locationCallback :LocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult?) {
-            locationResult ?: return
-            for (location in locationResult.locations){
-                val userLocationLatLng = LatLng(location.latitude, location.longitude)
-                if(mCurrLocationMarker == null) {
-                    val markerOptions = MarkerOptions().position(userLocationLatLng).title("Sua posição")
-                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.blue_circle))
-                    mCurrLocationMarker = mMap?.addMarker(markerOptions)
-                }
-                else mCurrLocationMarker?.position = userLocationLatLng
-                //mMap.clear();
 
-                //Marker currentLocationMarker = mMap.addMarker(new MarkerOptions().position(userLocation));
-//                    mMap?.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
-//                    mMap?.animateCamera(CameraUpdateFactory.zoomTo(12f))
-            }
-        }
-    }
+
     private lateinit var locationManager : LocationManager
     private var requestingLocationUpdates: Boolean = false
+    private var mCurrLocationMarker: Marker? = null
 
     private val REQUESTING_LOCATION_UPDATES_KEY: String = "request_updates_key"
     private var locationRequest: LocationRequest? = null
@@ -204,8 +182,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == RESULT_OK) {
-            if(requestCode == REQUEST_CHECK_LOCATION_SETTINGS){
-                requestingLocationUpdates = true
+            if(requestCode == REQUEST_CHANGE_LOCATION_SETTINGS){
+                userLocationManager?.createLocationRequest()
             }
             else {
                 Toast.makeText(applicationContext, "Atualizando recomendações...", Toast.LENGTH_SHORT).show()
@@ -221,8 +199,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
                 }
                 updateRecommender()
             }
-        }else if(requestCode == REQUEST_CHECK_LOCATION_SETTINGS){
-            requestingLocationUpdates = false
         }
     }
 
@@ -251,10 +227,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
 
     override fun onMapReady(p0: GoogleMap) {
         val locationListener : Int
-        if(p0 == null){
-            Toast.makeText(applicationContext, "Erro ao carregar o mapa.", Toast.LENGTH_SHORT).show()
-            throw Exception("null maps variable")
-        }
+
         mMap = p0
         mMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
         // Add a marker in Sydney and move the camera
@@ -267,8 +240,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
         mMap?.moveCamera(CameraUpdateFactory.newLatLng(LatLng(-23.651450,-46.622546)));
         //Initialize Location
         addItemsToMap()
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        createLocationRequest()
+        userLocationManager = UserLocationManager(this, REQUEST_CHANGE_LOCATION_SETTINGS){ userLatLng ->
+            if (mCurrLocationMarker == null) {
+                val markerOptions = MarkerOptions().position(userLatLng).title("Sua posição")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.blue_circle))
+                mCurrLocationMarker = mMap?.addMarker(markerOptions)
+            } else mCurrLocationMarker?.position = userLatLng
+        }
+
     }
 
     private fun addItemsToMap() {
@@ -280,93 +259,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
 
     override fun onResume() {
         super.onResume()
-        if (requestingLocationUpdates) startLocationUpdates()
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdates() {
-
-
-
-//        locationCallback = object :  LocationCallback() {
-//            override fun onLocationResult(locationResult: LocationResult?) {
-//                locationResult ?: return
-//                for (location in locationResult.locations){
-//                    val userLocation = LatLng(location.latitude, location.longitude)
-//                    val markerOptions = MarkerOptions()
-//                    markerOptions.position(userLocation)
-//                    markerOptions.title("Sua posição")
-//                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-//                    mCurrLocationMarker = mMap?.addMarker(markerOptions)
-//                    //mMap.clear();
-//
-//                    //Marker currentLocationMarker = mMap.addMarker(new MarkerOptions().position(userLocation));
-//                    mMap?.moveCamera(CameraUpdateFactory.newLatLng(userLocation))
-//                    mMap?.animateCamera(CameraUpdateFactory.zoomTo(12f))
-//                }
-//            }
-//        }
-        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            Toast.makeText(applicationContext, "need permission", Toast.LENGTH_SHORT).show()
-        mFusedLocationClient.requestLocationUpdates(locationRequest,
-                locationCallback,
-                null)
+        userLocationManager?.startLocationUpdates()
     }
 
     override fun onPause() {
         super.onPause()
-        stopLocationUpdates()
-    }
-
-    private fun stopLocationUpdates() {
-        mFusedLocationClient.removeLocationUpdates(locationCallback)
+        userLocationManager?.stopLocationUpdates()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
         if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                createLocationRequest()
+            if (ActivityCompat.checkSelfPermission(applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                userLocationManager?.createLocationRequest()
             }
 
         }
     }
 
-    private fun createLocationRequest() {
-        if (Build.VERSION.SDK_INT >= 23)
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
 
-        locationRequest = LocationRequest.create()?.apply {
-            interval = 1000
-            fastestInterval = 1000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-        locationRequest?.let {
-            val builder = LocationSettingsRequest.Builder()
-                    .addLocationRequest(it)
-            val client: SettingsClient = LocationServices.getSettingsClient(this)
-            val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-            task.addOnSuccessListener { locationSettingsResponse ->
-                requestingLocationUpdates =  true
-                startLocationUpdates()
-            }
-
-            task.addOnFailureListener { exception ->
-                if (exception is ResolvableApiException){
-                    // Location settings are not satisfied. Show to the user a dialog.
-                    try {
-                        // then check the result in onActivityResult().
-                        exception.startResolutionForResult(this@MainActivity,
-                                REQUEST_CHECK_LOCATION_SETTINGS)
-                    } catch (sendEx: IntentSender.SendIntentException) {
-                        // Ignore the error.
-                    }
-                }
-            }
-        }
-    }
 
     fun toggleSheet(v: View){
         if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED)
@@ -375,9 +287,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, ItemsGridListAdapt
             bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
     }
 
-    private fun setUserLocation() {
-        //@SuppressLint("MissingPermission") val lastKnowLocation = mFusedLocationClient.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-    }
 //
 //            //  mMap.clear();
 //            //            MarkerOptions mp = new MarkerOptions().icon(BitmapDescriptorFactory.defaultMarker());
---
+++ app/src/main/java/flaskoski/rs/smartmuseum/location/UserLocationManager.kt
@@ -0,0 +1,178 @@
+package flaskoski.rs.smartmuseum.model
+
+import android.app.Activity
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
+import java.lang.Exception
+
+/***
+ * UserLocationManager
+ * You have to override the activity's onPause, onResume and onRequestPermissionsResult.
+ *
+ * @param activity: current activity
+ * @param onUserLocationUpdateCallback: function that handles the user location returned
+ *
+ */
+class UserLocationManager(val activity: Activity, val REQUEST_CHANGE_LOCATION_SETTINGS : Int,  val onUserLocationUpdateCallback : (userLatLng: LatLng)-> Unit) : LocationCallback() {
+    private var locationRequest: LocationRequest? = null
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
+    init {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(activity)
+        createLocationRequest()
+    }
+    override fun onLocationResult(locationResult: LocationResult?) {
+        locationResult ?: return
+        for (location in locationResult.locations) {
+            val userLatLng = LatLng(location.latitude, location.longitude)
+            onUserLocationUpdateCallback(userLatLng)
+        }
+    }
+
+    fun stopLocationUpdates() {
+        mFusedLocationClient?.removeLocationUpdates(this)
+    }
+
+    fun startLocationUpdates() {
+        if(ActivityCompat.checkSelfPermission(activity.applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(activity.applicationContext, "Erro: O aplicativo precisa de permissão para acessar sua localização para funcionar.", Toast.LENGTH_SHORT).show()
+            throw Exception("location permission not granted.")
+        }
+        if(!locationSettingsCorrect){
+            Toast.makeText(activity.applicationContext, "Erro: Configuração de localização incorreta", Toast.LENGTH_SHORT).show()
+            throw Exception("Wrong phone location settings.")
+        }
+        mFusedLocationClient!!.requestLocationUpdates(locationRequest,
+                this,
+                null)
+    }
+
+    private var locationSettingsCorrect: Boolean = false
+
+    fun createLocationRequest() {
+        if (Build.VERSION.SDK_INT >= 23)
+            if (ContextCompat.checkSelfPermission(activity.applicationContext, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(activity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+
+        locationRequest = LocationRequest.create()?.apply {
+            interval = 1000
+            fastestInterval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        locationRequest?.let {
+            val builder = LocationSettingsRequest.Builder()
+                    .addLocationRequest(it)
+            val client: SettingsClient = LocationServices.getSettingsClient(activity.applicationContext)
+            val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+            task.addOnSuccessListener { locationSettingsResponse ->
+                locationSettingsCorrect =  true
+                startLocationUpdates()
+            }
+
+            task.addOnFailureListener { exception ->
+                if (exception is ResolvableApiException){
+                    // Location settings are not satisfied. Show to the user a dialog.
+                    try {
+                        // then check the result in onActivityResult().
+                        exception.startResolutionForResult(activity,
+                                REQUEST_CHANGE_LOCATION_SETTINGS)
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+//package flaskoski.rs.smartmuseum.location
+//
+//import android.location.Location;
+//import android.os.Looper;
+//import android.util.Log;
+//
+//import com.google.android.gms.location.FusedLocationProviderClient;
+//import com.google.android.gms.location.LocationCallback;
+//import com.google.android.gms.location.LocationRequest;
+//import com.google.android.gms.location.LocationResult;
+//import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.location.LocationSettingsRequest;
+//
+///**
+// * Uses Google Play API for obtaining device locations
+// * Created by alejandro.tkachuk
+// * alejandro@calculistik.com
+// * www.calculistik.com Mobile Development
+// */
+//
+//class UserLocationManager {
+//    companion object {
+//        val instance : UserLocationManager = UserLocationManager();
+//
+//        val TAG = UserLocationManager::class.java.simpleName;
+//
+//    }
+//    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+//    private lateinit var locationCallback : LocationCallback
+//    private lateinit var locationRequest : LocationRequest
+//    private lateinit var locationSettingsRequest : LocationSettingsRequest
+//
+//    private lateinit var workable : Workable<GPSPoint>
+//
+//    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+//    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 1000;
+//
+//    private Wherebouts() {
+//        this.locationRequest = new LocationRequest();
+//        this.locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+//        this.locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+//        this.locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//
+//        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+//        builder.addLocationRequest(this.locationRequest);
+//        this.locationSettingsRequest = builder.build();
+//
+//        this.locationCallback = new LocationCallback() {
+//            @Override
+//            public void onLocationResult(LocationResult locationResult) {
+//                super.onLocationResult(locationResult); // why? this. is. retarded. Android.
+//                Location currentLocation = locationResult.getLastLocation();
+//
+//                GPSPoint gpsPoint = new GPSPoint(currentLocation.getLatitude(), currentLocation.getLongitude());
+//                Log.i(TAG, "Location Callback results: " + gpsPoint);
+//                if (null != workable)
+//                    workable.work(gpsPoint);
+//            }
+//        };
+//
+//        this.mFusedLocationClient = LocationServices.getFusedLocationProviderClient(MainApplication.getAppContext());
+//        this.mFusedLocationClient.requestLocationUpdates(this.locationRequest,
+//                this.locationCallback, Looper.myLooper());
+//    }
+//
+//    public static Wherebouts instance() {
+//        return instance;
+//    }
+//
+//    public void onChange(Workable<GPSPoint> workable) {
+//        this.workable = workable;
+//    }
+//
+//    public LocationSettingsRequest getLocationSettingsRequest() {
+//        return this.locationSettingsRequest;
+//    }
+//
+//    public void stop() {
+//        Log.i(TAG, "stop() Stopping location tracking");
+//        this.mFusedLocationClient.removeLocationUpdates(this.locationCallback);
+//    }
+//
+//}
